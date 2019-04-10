d0ac5f1f4839a5350b90ffb4be5bb512d30f9349
iammarkps <markkung08@gmail.com>
2019-04-10 22:23:16 +0700

    5 4 3 2 1.... Fade in

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ particlesJS.load("particles-js", "assets/particles-config.json", function() {
 });
 
 var rellax = new Rellax(".rellax");
+var lyric = document.getElementById("lyric");
+
+window.addEventListener("scroll", function() {
+  if (lyric.inViewport(0.5, 0.5)) {
+    lyric.classList.replace("hidden", "fade");
+  }
+});
