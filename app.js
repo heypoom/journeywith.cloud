ae7897a18f0bf98f7d01472916bcbaf372499607
iammarkps <markkung08@gmail.com>
2019-04-16 16:14:07 +0700

    Latest experiment

--- app.js
+++ app.js
@@ -17,33 +17,27 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000);
+renderer.autoClear = false;
+renderer.setClearColor(0xffffff, 0.0);
 
 document.getElementById("canvas").appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const starGeometry = new THREE.Geometry();
 
-const light = new THREE.HemisphereLight(0xffffff, 0xffffff, 100);
+for (let i = 0; i < 112000; i++) {
+  const star = new THREE.Vector3();
+  star.x = THREE.Math.randFloatSpread(1200);
+  star.y = THREE.Math.randFloatSpread(1200);
+  star.z = THREE.Math.randFloatSpread(1200);
+  starGeometry.vertices.push(star);
+}
 
-// const starGeometry = new THREE.Geometry();
+const starsMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+const starField = new THREE.Points(starGeometry, starsMaterial);
 
-// for (let i = 0; i < 112≠000; i++) {
-//   const star = new THREE.Vector3();
-//   star.x = THREE.Math.randFloatSpread(1200);
-//   star.y = THREE.Math.randFloatSpread(1200);
-//   star.z = THREE.Math.randFloatSpread(1200);
-//   starGeometry.vertices.push(star);
-// }
-
-// const starsMaterial = new THREE.PointsMaterial({ color: 0xffffff });
-// const starField = new THREE.Points(starGeometry, starsMaterial);
-
-scene.add(light);
+scene.add(starField);
 
 renderer.render(scene, camera);
